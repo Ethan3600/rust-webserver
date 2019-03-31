c4101c5cbe9354ddcd892ab8def5c81acc16036c
ethan3600 <ethan3600@gmail.com>
2019-03-31 19:52:48 -0400

    Create response from server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_stream(stream);
+    }
+}
+
+fn handle_stream(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
