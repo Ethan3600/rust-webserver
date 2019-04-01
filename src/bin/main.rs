955b3758ea476af1f8a07f26d89657833b12062e
ethan3600 <ethan3600@gmail.com>
2019-03-31 20:45:29 -0400

    Change binary crate to library

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,6 +5,8 @@ use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 
+use webserver::ThreadPool;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
